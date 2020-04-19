a0ca26ac347688689bf531a0672dab2c5a519617
houcine.krichen <houcine.krichen@gmail.com>
2020-04-19 18:08:33 +0200

    update application startup

--- src/main/java/com/example/reactivemongo/util/ApplicationStartup.java
+++ src/main/java/com/example/reactivemongo/util/ApplicationStartup.java
@@ -1,37 +1,26 @@
 package com.example.reactivemongo.util;
 
-import com.example.reactivemongo.entity.Level;
 import com.example.reactivemongo.entity.Log;
-import com.example.reactivemongo.service.LogService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.data.mongodb.UncategorizedMongoDbException;
 import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 @Component
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
 
     private final MongoOperations mongoOperations;
-    private final LogService service;
 
-    public ApplicationStartup(MongoOperations mongoOperations, LogService service) {
+    public ApplicationStartup(MongoOperations mongoOperations) {
         this.mongoOperations = mongoOperations;
-        this.service = service;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        try {
-            mongoOperations.createCollection(Log.class, CollectionOptions.empty().capped().size(102400).maxDocuments(10));
-        }catch (UncategorizedMongoDbException ex) {
-            service.save(new Log()
-                    .service(ApplicationStartup.class.getTypeName())
-                    .message("Collection already exist").level(Level.ERROR)
-                    .date(LocalDateTime.now())).subscribe();
+        if (!mongoOperations.collectionExists(Log.class)) {
+            mongoOperations.createCollection(Log.class, CollectionOptions.empty()
+                    .capped().size(1024 * 1024).maxDocuments(100));
         }
     }
 }
